d611a996be257dc1eafcfc0a38f735fcf8a814a3
Robert Bishop <rbishop2006@gmail.com>
2020-03-09 07:58:10 -0700

    basicStyling

--- client/src/components/subCat.js
+++ client/src/components/subCat.js
@@ -26,34 +26,44 @@ export default props => {
   }
 
   return (
-    <div>
-      <div className="">
+    <div className="">
+      <div className="subCatsHeaderWrapper">
         <h1>{listings.length > 0 && listings[0].name}</h1>
-        {listings.map(listing => (
-          <div className="" key={"listing" + listing.id}>
-            <Link
-              onClick={e => fetchListing(listing.id)}
-              to={{ pathname: "/listing/" + listing.id }}
-            >
-              <h3 className="">{listing.listing_name}</h3>
-            </Link>
-          </div>
-        ))}
+        <h1>post a new listing</h1>
       </div>
-      <form>
-        <input
-          type="text"
-          placeholder="Title"
-          onChange={e => setTitle(e.target.value)}
-          value={title}
-        />
-        <textarea
-          placeholder="Description"
-          onChange={e => setDesc(e.target.value)}
-          value={desc}
-        ></textarea>
-        <button onSubmit={handleSubmit}>Submit</button>
-      </form>
+      <div className="subCatWrapper">
+        <div className="subCatInfo">
+          {listings.map(listing => (
+            <div className="subCatTextInfo" key={"listing" + listing.id}>
+              <Link
+                className="subCatLinkInfo"
+                onClick={e => fetchListing(listing.id)}
+                to={{ pathname: "/listing/" + listing.id }}
+              >
+                <h3 className="subCatTextInfo">{listing.listing_name}</h3>
+              </Link>
+            </div>
+          ))}
+        </div>
+        <form className="subCatForm">
+          <input
+            className="subCatFormTitle"
+            type="text"
+            placeholder="ex. canoeing, running, hiking, etc."
+            onChange={e => setTitle(e.target.value)}
+            value={title}
+          />
+          <textarea
+            className="subCatFormTextarea"
+            placeholder="ex. Canoeing is my passion, I'd like to find a group to organize trips ...."
+            onChange={e => setDesc(e.target.value)}
+            value={desc}
+          ></textarea>
+          <button className="subCatFormSubmit" onSubmit={handleSubmit}>
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
