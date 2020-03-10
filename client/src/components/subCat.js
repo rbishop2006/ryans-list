b62630bf71c6ae55b84a987c91688664f1c2abff
Robert Bishop <rbishop2006@gmail.com>
2020-03-10 15:38:26 -0700

    addedValidator

--- client/src/components/subCat.js
+++ client/src/components/subCat.js
@@ -3,14 +3,19 @@ import { useListings, useListing } from "../hooks"
 import "../styles/subCat.css"
 import { Link } from "react-router-dom"
 import moment from "moment"
+import validator from "validator"
 export default props => {
   const { listings, fetchListings, sendListing } = useListings()
-
   const [title, setTitle] = useState("")
-  const [desc, setDesc] = useState("")
+  const [titleError, setTitleError] = useState("")
   const [city, setCity] = useState("")
+  const [cityError, setCityError] = useState("")
   const [location, setLocation] = useState("")
+  const [locationError, setLocationError] = useState("")
   const [price, setPrice] = useState("")
+  const [priceError, setPriceError] = useState("")
+  const [desc, setDesc] = useState("")
+  const [descError, setDescError] = useState("")
 
   useEffect(() => {
     fetchListings(props.match.params.slug)
@@ -18,12 +23,50 @@ export default props => {
 
   function handleSubmit(e) {
     e.preventDefault()
-    sendListing(props.match.params.slug, title, desc, city, location, price)
-    setTitle("")
-    setDesc("")
-    setCity("")
-    setLocation("")
-    setPrice("")
+
+    let valid = true
+
+    if (validator.isEmpty(title)) {
+      valid = false
+      setTitleError(
+        "-- Cannot be blank, must contain letters and/or numbers only"
+      )
+    } else {
+      setTitleError("")
+    }
+    if (validator.isEmpty(city)) {
+      valid = false
+      setCityError("-- Cannot be blank, must contain letters only")
+    } else {
+      setCityError("")
+    }
+    if (validator.isEmpty(location)) {
+      valid = false
+      setLocationError("-- Cannot be blank, must contain letters only")
+    } else {
+      setLocationError("")
+    }
+    if (validator.isEmpty(price)) {
+      setPriceError(
+        "-- Cannot be blank, must contain letters and/or numbers only"
+      )
+    } else {
+      setPriceError("")
+    }
+    if (validator.isEmpty(desc)) {
+      setDescError("-- Cannot be blank, must contain letters/numbers only")
+    } else {
+      setDescError("")
+    }
+
+    if (valid) {
+      sendListing(props.match.params.slug, title, desc, city, location, price)
+      setTitle("")
+      setCity("")
+      setLocation("")
+      setPrice("")
+      setDesc("")
+    }
   }
 
   return (
@@ -51,61 +94,63 @@ export default props => {
           ))}
         </div>
         <form onSubmit={handleSubmit} className="subCatForm">
-          <label className="subCatLabels" htmlFor="title">
-            title
+          <label className={titleError ? "error" : ""} htmlFor="title">
+            title {titleError && titleError}
           </label>
           <input
-            className="subCatFormTitle"
+            className={titleError ? "errorBox" : ""}
             id="title"
             type="text"
             placeholder="ex. canoeing, running, hiking, etc."
             onChange={e => setTitle(e.target.value)}
             value={title}
           />
-          <label className="subCatLabels" htmlFor="city">
-            city
+          <label className={cityError ? "error" : ""} htmlFor="city">
+            city {cityError && cityError}
           </label>
           <input
-            className="subCatFormTitle"
+            className={cityError ? "errorBox" : ""}
             id="city"
             type="text"
             placeholder="ex. Las Vegas"
             onChange={e => setCity(e.target.value)}
             value={city}
           />
-          <label className="subCatLabels" htmlFor="location">
-            location
+          <label className={locationError ? "error" : ""} htmlFor="location">
+            location {locationError && locationError}
           </label>
           <input
-            className="subCatFormTitle"
+            className={locationError ? "errorBox" : ""}
             id="location"
             type="text"
             placeholder="ex. Summerlin"
             onChange={e => setLocation(e.target.value)}
             value={location}
           />
-          <label className="subCatLabels" htmlFor="price">
-            price
+          <label className={priceError ? "error" : ""} htmlFor="price">
+            price {priceError && priceError}
           </label>
           <input
-            className="subCatFormPrice"
+            className={priceError ? "errorBox" : ""}
             id="price"
             type="text"
             placeholder="ex. Free or $5.00 per class"
             onChange={e => setPrice(e.target.value)}
             value={price}
           ></input>
-          <label className="subCatLabels" htmlFor="Description">
-            description
+          <label className={descError ? "error" : ""} htmlFor="Description">
+            description {descError && descError}
           </label>
           <textarea
-            className="subCatFormTextarea"
+            className={descError ? "errorBox" : ""}
             id="Description"
             placeholder="ex. Canoeing is my passion, I'd like to find a group to organize trips ...."
             onChange={e => setDesc(e.target.value)}
             value={desc}
           ></textarea>
-          <button className="subCatFormSubmit">Submit</button>
+          <button type="submit" className="subCatFormSubmit">
+            Submit
+          </button>
         </form>
       </div>
       <div>
